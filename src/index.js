2bcc7222ee074eb4c022eb1c154abfb32950f539
Sen4az <simon.z@yandex.ru>
2018-05-11 01:13:43 +0300

    addListener start

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@
  Функция должна добавлять обработчик fn события eventName к элементу target
 
  Пример:
-   addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
+   addListener('click', document.querySelector('a'), () => console.log('...'))
+   // должна добавить указанный обработчик кликов на указанный элемент
  */
 function addListener(eventName, target, fn) {
+    target.addEventListener(eventName, fn());
 }
 
 /*
@@ -17,7 +19,8 @@ function addListener(eventName, target, fn) {
  Функция должна удалять у элемента target обработчик fn события eventName
 
  Пример:
-   removeListener('click', document.querySelector('a'), someHandler) // должна удалить указанный обработчик кликов на указанный элемент
+   removeListener('click', document.querySelector('a'), someHandler)
+   // должна удалить указанный обработчик кликов на указанный элемент
  */
 function removeListener(eventName, target, fn) {
 }
@@ -28,7 +31,8 @@ function removeListener(eventName, target, fn) {
  Функция должна добавить к элементу target такой обработчик на события eventName, чтобы он отменял действия по умолчанию
 
  Пример:
-   skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
+   skipDefault('click', document.querySelector('a')) // после вызова функции,
+   клики на указанную ссылку не должны приводить к переходу на другую страницу
  */
 function skipDefault(eventName, target) {
 }
@@ -51,7 +55,9 @@ function emulateClick(target) {
  который реагирует (вызывает fn) только на клики по элементам BUTTON внутри target
 
  Пример:
-   delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
+   delegate(document.body, () => console.log('кликнули на button')) // добавит такой
+   обработчик кликов для body, который будет вызывать указанную функцию только если
+   кликнули на кнопку (элемент с тегом button)
  */
 function delegate(target, fn) {
 }
@@ -63,7 +69,9 @@ function delegate(target, fn) {
  который сработает только один раз и удалится (перестанет срабатывать для последующих кликов по указанному элементу)
 
  Пример:
-   once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
+   once(document.querySelector('button'), () => console.log('обработчик
+   выполнился!')) // добавит такой обработчик кликов для указанного элемента,
+   который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
 }
