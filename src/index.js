852d3f5c3839efda1029477a76033b62ca79a123
szadorozhniy <szadorozhniy@axiomsl.com>
2018-05-16 13:29:29 +0300

    6.1 done
    (cherry picked from commit 4e53005)

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
 function delayPromise(seconds) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, seconds);
+    });
 }
 
 /*
@@ -25,6 +30,32 @@ function delayPromise(seconds) {
    loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
 function loadAndSortTowns() {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        
+        xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+        xhr.responseType = 'json';
+        xhr.send();
+        xhr.addEventListener('load', () => {
+            if (xhr.status >= 400) {
+                reject();
+            } else {
+                const cities = xhr.response;
+                const sortCities = cities.sort((a,b) => {
+                    if(a.name > b.name) {
+                        return 1;
+                    }
+                    if(a.name < b.name) {
+                        return -1;
+                    }
+                    return 0;
+                });
+                resolve(sortCities);
+            }
+        });
+        xhr.addEventListener('error', reject);
+        xhr.addEventListener('abort', reject);
+    });
 }
 
 export {
